e4d56f6e7640922a6bef5a180297cb2d11858ba7
Ilya Boyandin <ilya@boyandin.me>
2022-05-24 14:35:44 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {Map as ReactMapGLMap} from 'react-map-gl';
+import DeckGL, {ArcLayer} from 'deck.gl';
+
+const MAP_STYLE = 'https://basemaps.cartocdn.com/gl/positron-nolabels-gl-style/style.json';
+
+const AIR_PORTS =
+  'https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_10m_airports.geojson';
+
+const INITIAL_VIEW_STATE = {
+  latitude: 51.47,
+  longitude: 0.45,
+  zoom: 4,
+  bearing: 0,
+  pitch: 30
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DeckGL controller={true} initialViewState={INITIAL_VIEW_STATE}>
+      <ReactMapGLMap
+        mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
+        mapStyle={MAP_STYLE}
+      />
+      <ArcLayer
+        id="arcs"
+        data={AIR_PORTS}
+        dataTransform={d => d.features.filter(f => f.properties.scalerank < 4)}
+        getSourcePosition={f => [-0.4531566, 51.4709959]}
+        getTargetPosition={f => f.geometry.coordinates}
+        getSourceColor={[0, 128, 200]}
+        getTargetColor={[200, 0, 80]}
+        getWidth={1}
+      />
+    </DeckGL>
   );
 }
+
 
 export default App;
