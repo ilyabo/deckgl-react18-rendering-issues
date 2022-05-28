f618a96dbabfc512d29397e3fd45c4efddfa90a4
Ilya Boyandin <ilya@boyandin.me>
2022-05-28 16:25:24 +0200

    Add zustand

--- src/App.js
+++ src/App.js
@@ -1,35 +1,79 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Map as ReactMapGLMap} from 'react-map-gl';
-import DeckGL, {ArcLayer} from 'deck.gl';
+import DeckGL, {ScatterplotLayer} from 'deck.gl';
+import {flushSync} from "react-dom";
+import create from "zustand";
 
 const MAP_STYLE = 'https://basemaps.cartocdn.com/gl/positron-nolabels-gl-style/style.json';
-
-const AIR_PORTS =
-  'https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_10m_airports.geojson';
+// const MAP_STYLE = 'mapbox://styles/mapbox/streets-v11';
 
 const INITIAL_VIEW_STATE = {
-  latitude: 51.47,
-  longitude: 0.45,
-  zoom: 4,
+  longitude: 0,
+  latitude: 0,
+  zoom: 2,
   bearing: 0,
-  pitch: 30
+  pitch: 0
 };
 
+const DATA = (() => {
+  const data = [];
+  for (let i = 0; i < 5000; i++) {
+    const lon = Math.random() * 360 - 180;
+    const lat = Math.random() * 180 - 90;
+    data.push({
+      coords: [lon, lat],
+      size: Math.random() * 10,
+      color: [Math.random() * 255, Math.random() * 255, Math.random() * 255]
+    });
+  }
+  return data;
+})();
+
+
+const useStore = create(set => ({
+  viewState: null,
+  setViewState: (viewState) => set(state => ({ viewState })),
+}))
+
 function App() {
+  const layers = [
+    new ScatterplotLayer({
+      data: DATA,
+      radiusUnits: 'pixels',
+      pickable: true,
+      opacity: 0.8,
+      filled: true,
+      getPosition: (d) => d.coords,
+      getRadius: (d) => d.size,
+      getFillColor: (d) => d.color,
+    }),
+
+  ]
+
+  const viewState_ = useStore(state => state.viewState)
+  const setViewState_ = useStore(state => state.setViewState)
+  const [viewState, setViewState] = useState(INITIAL_VIEW_STATE);
+
+  // useEffect(() => {
+  //   setViewState_(viewState);
+  // }, [viewState]);
+
+  const handleViewStateChange = ({viewState}) => {
+    // flushSync(() => {
+    setViewState_(viewState);
+    // });
+  };
+
   return (
-    <DeckGL controller={true} initialViewState={INITIAL_VIEW_STATE}>
+    <DeckGL
+      controller={true}
+      initialViewState={INITIAL_VIEW_STATE}
+      onViewStateChange={handleViewStateChange}
+      viewState={viewState_}
+      layers={layers}
+    >
       <ReactMapGLMap
         mapStyle={MAP_STYLE}
-      />
-      <ArcLayer
-        id="arcs"
-        data={AIR_PORTS}
-        dataTransform={d => d.features.filter(f => f.properties.scalerank < 4)}
-        getSourcePosition={f => [-0.4531566, 51.4709959]}
-        getTargetPosition={f => f.geometry.coordinates}
-        getSourceColor={[0, 128, 200]}
-        getTargetColor={[200, 0, 80]}
-        getWidth={1}
       />
     </DeckGL>
   );
